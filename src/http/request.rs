451a338311db1b626853f58890770c38d9555a65
n0pj <admin@n0pj.com>
2022-02-15 19:45:24 +0900

    find_param 追加

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,59 +2,48 @@ use ncms_core::errors;
 use ncms_core::errors::http::{ValueError, ValueErrors};
 use regex::Regex;
 use serde_json::{to_value, Value};
+use std::io::{Error, ErrorKind};
 
 /// Lambda に GET パラメーターを渡した場合 queryStringParameters に入る。
 /// そこから GET パラメーターを取得する
-pub fn get_query(event: Value) -> Result<Value, Value> {
+pub fn get_query(event: Value) -> Result<Value, Error> {
     // println!("{:?}", event);
 
     // Lambda では queryStringParameters の中に GET パラメーターが入る
     let query_string_parameters = match event.get("queryStringParameters") {
         Some(event) => event.clone(),
         None => {
-            let field_error = ValueError {
-                property: Some(
-                    errors::validation::CANNOT_FIND_QUERY_STRING_PARAMETERS
-                        .message
-                        .to_owned(),
-                ),
-                ..Default::default()
-            };
-            let field_errors = ValueErrors::new(vec![field_error]);
-
-            return Err(to_value(field_errors).expect("fatal error"));
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "queryStringParameters is not found",
+            ))
         }
     };
 
     // query に GraphQL query を入れるため、ここから取得
     match query_string_parameters.get("query") {
         Some(query) => Ok(query.clone()),
-        None => {
-            let field_error = ValueError {
-                property: Some(errors::validation::CANNOT_FIND_QUERY.message.to_owned()),
-                ..Default::default()
-            };
-            let field_errors = ValueErrors::new(vec![field_error]);
+        None => Err(Error::new(ErrorKind::InvalidInput, "query is not found")),
+    }
+}
 
-            return Err(to_value(field_errors).expect("fatal error"));
-        }
+/// 指定のパラメーターがあるかどうかを確認し、あればそのパラメーターを返す
+pub fn find_param(event: Value, param: &str) -> Result<Value, Error> {
+    match event.get(param) {
+        Some(event) => Ok(event.clone()),
+        None => Err(Error::new(
+            ErrorKind::InvalidInput,
+            format!("{} is not found", param),
+        )),
     }
 }
 
 /// GET で送られてきたものは "" で囲まれてしまうため、 "" を解除する
 /// "query { humans(i: 0) { name } }" -> query { humans(i: 0) { name } }
-pub fn format_query(query: Value) -> Result<String, Value> {
+pub fn format_query(query: Value) -> Result<String, Error> {
     let query = match serde_json::to_string(&query) {
         Ok(result) => result,
-        Err(_) => {
-            let field_error = ValueError {
-                property: Some(errors::validation::CANNOT_FIND_QUERY.message.to_owned()),
-                ..Default::default()
-            };
-            let field_errors = ValueErrors::new(vec![field_error]);
-
-            return Err(field_errors.to_value());
-        }
+        Err(_) => return Err(Error::new(ErrorKind::InvalidInput, "query is not found")),
     };
     // let query = query.to_string();
     let re = Regex::new(r#""(.*)""#).unwrap();
@@ -62,4 +51,34 @@ pub fn format_query(query: Value) -> Result<String, Value> {
     let query = caps.get(1).map_or("", |m| m.as_str());
 
     Ok(query.to_owned())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_get_query() {
+        let event = json!({
+            "queryStringParameters": {
+                "query": "query { humans(i: 0) { name } }"
+            }
+        });
+        let query = get_query(event).unwrap();
+        let query = format_query(query).unwrap();
+        assert_eq!(query, "query { humans(i: 0) { name } }");
+    }
+
+    #[test]
+    fn test_find_param() {
+        let event = json!({
+            "queryStringParameters": {
+                "query": "query { humans(i: 0) { name } }"
+            }
+        });
+        let query = get_query(event).unwrap();
+        let query = format_query(query).unwrap();
+        assert_eq!(query, "query { humans(i: 0) { name } }");
+    }
 }
